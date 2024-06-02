427fb2fe8d0c0063bf03c998049037209c542ff5
Hisham <hishamsaeed2003@gmail.com>
2024-06-02 10:23:00 +0530

    parallax background

---
+++ frontend/src/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Hero.css";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <h1 className="hero-title">LernQuick</h1>
+        <p className="hero-message">Learning made Faster</p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,8 +1,7 @@
-// src/Home.jsx
 import React from "react";
 import { Link } from "react-router-dom";
-import NavBar from "./Home-components/NavBar";
-import ImageSlider from "./Home-components/ImageSlider";
+import NavBar from "./NavBar";
+import Hero from "./Hero";
 import "./Home.css";
 
 const blogs = [
@@ -16,6 +15,36 @@ const blogs = [
     title: "Blog Post 2",
     excerpt: "This is a short description of blog post 2.",
   },
+  {
+    id: 3,
+    title: "Blog Post 3",
+    excerpt: "This is a short description of blog post 3.",
+  },
+  {
+    id: 4,
+    title: "Blog Post 4",
+    excerpt: "This is a short description of blog post 4.",
+  },
+  {
+    id: 5,
+    title: "Blog Post 5",
+    excerpt: "This is a short description of blog post 5.",
+  },
+  {
+    id: 6,
+    title: "Blog Post 6",
+    excerpt: "This is a short description of blog post 6.",
+  },
+  {
+    id: 7,
+    title: "Blog Post 7",
+    excerpt: "This is a short description of blog post 7.",
+  },
+  {
+    id: 8,
+    title: "Blog Post 8",
+    excerpt: "This is a short description of blog post 8.",
+  },
   // Add more blog posts as needed
 ];
 
@@ -23,19 +52,22 @@ function Home() {
   return (
     <div className="home">
       <NavBar />
-      <ImageSlider />
-      <div className="intro">
-        <h1>Welcome to the Blog</h1>
-        <p>This is an introduction to the blog. Enjoy reading!</p>
-      </div>
-      <div className="cards">
-        {blogs.map((blog) => (
-          <div key={blog.id} className="card">
-            <h2>{blog.title}</h2>
-            <p>{blog.excerpt}</p>
-            <Link to={`/blog/${blog.id}`}>Read More</Link>
-          </div>
-        ))}
+      <Hero />
+      <div className="background-image"></div>
+      <div className="content">
+        <div className="intro">
+          <h1>Welcome to the Blog</h1>
+          <p>This is an introduction to the blog. Enjoy reading!</p>
+        </div>
+        <div className="cards">
+          {blogs.map((blog) => (
+            <div key={blog.id} className="card">
+              <h2>{blog.title}</h2>
+              <p>{blog.excerpt}</p>
+              <Link to={`/blog/${blog.id}`}>Read More</Link>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
