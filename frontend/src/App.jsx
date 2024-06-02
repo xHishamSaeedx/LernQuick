3cd72fa0477f1b1a065a816c5cd197a14f6e4780
Hisham <hishamsaeed2003@gmail.com>
2024-06-02 14:15:50 +0530

    fade off landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,39 @@
-// App.jsx
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Blog from "./Blog";
 import "./App.css";
 
 function App() {
+  const [showAnimation, setShowAnimation] = useState(true);
+
+  useEffect(() => {
+    // Simulate the animation duration
+    const animationTimeout = setTimeout(() => {
+      setShowAnimation(false);
+    }, 3000); // Adjust the duration as needed
+
+    return () => clearTimeout(animationTimeout);
+  }, []);
+
   return (
     <Router>
       <div className="app">
+        {showAnimation && (
+          <div className="animation-container">
+            <div className="wallpaper">
+              <h1
+                style={{
+                  textShadow:
+                    "0 0 10px #ff7f00, 0 0 20px #ff7f00, 0 0 30px #ff7f00",
+                }}
+                className="welcome-text"
+              >
+                Welcome
+              </h1>
+            </div>
+          </div>
+        )}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/blog/:id" element={<Blog />} />
@@ -19,13 +44,3 @@ function App() {
 }
 
 export default App;
-
-// function App() {
-//   const [count, setCount] = useState(0);
-//   const [message, setMessage] = useState("Loading...");
-
-//   useEffect(() => {
-//     fetch("/api/data")
-//       .then((response) => response.json())
-//       .then((data) => setMessage(data.message));
-//   }, []);
