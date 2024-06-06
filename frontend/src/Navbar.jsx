b26d8e0c22742ed1a3b3a6634ed1b5f2342f6fce
Hisham <hishamsaeed2003@gmail.com>
2024-06-06 19:22:04 +0530

    working prototype of responsive navbar

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,5 +1,5 @@
 // Navbar.jsx
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./Navbar.css";
 
@@ -9,6 +9,7 @@ const Navbar = ({
   handleScrollToContact,
   handleScrollToCourses,
 }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
 
   const navigateAndScroll = (scrollToFunction) => {
@@ -16,23 +17,52 @@ const Navbar = ({
     setTimeout(scrollToFunction, 100); // Adjust timeout as needed
   };
 
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
-      <h1 onClick={() => navigateAndScroll(handleScrollToTop)}>LERNQUICK</h1>
-      <ul>
-        <li onClick={() => navigateAndScroll(handleScrollToTop)}>
-          <Link to="/">Home</Link>
-        </li>
-        <li onClick={() => navigateAndScroll(handleScrollToAbout)}>
-          <Link to="/">About</Link>
-        </li>
-        <li onClick={() => navigateAndScroll(handleScrollToContact)}>
-          <Link to="/">Contact</Link>
-        </li>
-        <li onClick={() => navigateAndScroll(handleScrollToCourses)}>
-          <Link to="/">Pick a Course</Link>
-        </li>
-      </ul>
+      <div className="navbar-container">
+        <h1 onClick={() => navigateAndScroll(handleScrollToTop)}>LERNQUICK</h1>
+        <div className="hamburger" onClick={toggleMenu}>
+          ☰
+        </div>
+        <ul className={menuOpen ? "menu open" : "menu"}>
+          <li
+            onClick={() => {
+              navigateAndScroll(handleScrollToTop);
+              toggleMenu();
+            }}
+          >
+            <Link to="/">Home</Link>
+          </li>
+          <li
+            onClick={() => {
+              navigateAndScroll(handleScrollToAbout);
+              toggleMenu();
+            }}
+          >
+            <Link to="/">About</Link>
+          </li>
+          <li
+            onClick={() => {
+              navigateAndScroll(handleScrollToContact);
+              toggleMenu();
+            }}
+          >
+            <Link to="/">Contact</Link>
+          </li>
+          <li
+            onClick={() => {
+              navigateAndScroll(handleScrollToCourses);
+              toggleMenu();
+            }}
+          >
+            <Link to="/">Pick a Course</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
